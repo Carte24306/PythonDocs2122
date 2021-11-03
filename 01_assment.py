838a6be423a7b88cfbf28b01b807268f2c6e6ace
Carte24306 <79584427+Carte24306@users.noreply.github.com>
2021-11-03 10:43:15 -0400

    Create 01_assment.py

---
+++ 01_assment.py
@@ -0,0 +1,39 @@
+#Carter Sprinkle
+#Assment Examples
+
+# You can asssign "values" to "variables" by useing an equals (right side gose into left side)
+x = 5
+
+#when python reads a var name, it replases it with the variable's stored value
+y = x + 5
+
+
+# there are 4 diffent primitive datatypes
+#integers: any whole number + or -
+age = 16
+# Float: any number with a decimal, + or -
+Grade = 98.6
+#string: a string of human readable characters
+name = "Chris"
+# numbers in a string are not numbers, there are letters
+favoriteNumber = "69"
+# boolean: True or False
+# True is any value that is not false or empty
+issmart = True
+#You can output to rhw console by useing 'print()'
+print(age)
+print(Grade)
+print(name)
+print(favoriteNumber)
+print (issmart)
+
+#you can concatenat simliar values togather
+print ("My Name" + name)
+#you can use funtctons to convert data types
+print ("and my age is" + str(age))
+#dont forget!!! If you want to convert a value permanently, You must assign the converted value to a variable
+age = str(age)
+#you can convert back and forth with int(), str(), bool(), and float
+print(int(age))
+print(float(age))
+print(bool(age))
